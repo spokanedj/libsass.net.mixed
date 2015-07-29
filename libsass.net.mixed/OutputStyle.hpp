ae54db1fc9dc7c60bdef26603b36695f6f36315e
unknown <h.roeland@Airen.capitalid.nl>
2015-07-29 23:26:23 +0200

    Added missing namespace

--- libsass.net.mixed/OutputStyle.hpp
+++ libsass.net.mixed/OutputStyle.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
-public enum class OutputStyle
+namespace LibSassNet
 {
-	Nested = 0,
-	Expanded = 1,
-	Compact = 2,
-	Compressed = 3,
-	Echo = 4
-};
 
+	public enum class OutputStyle
+	{
+		Nested = 0,
+		Expanded = 1,
+		Compact = 2,
+		Compressed = 3,
+		Echo = 4
+	};
+
+}
